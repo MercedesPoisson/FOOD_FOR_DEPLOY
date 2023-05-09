b79af0ac695fe487aaa92ea86d1535fa6b181796
MercedesPoisson <mercedespoisson@yahoo.com>
2023-05-09 00:17:13 -0300

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
-import React from 'react';
-
 
 function App() {
   return (
     <div className="App">
-    {/* <img className="background-img" src={require('./background.jpg')} alt='Food Background' /> */}
       <h1>Henry Food</h1>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -3,18 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from "./redux/store";
-import { Provider } from "react-redux";
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <Provider store={store}>
-<BrowserRouter>
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-  </BrowserRouter>
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
