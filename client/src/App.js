ac29357610f0fbb54c16a36781e1fef9274c8d73
MercedesPoisson <mercedespoisson@yahoo.com>
2023-05-10 16:59:02 -0300

    Empezando el Front

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
+import { Route } from 'react-router-dom';
 import './App.css';
+import Home from "./views/Home/Home";
+import Landing from './views/Landing/Landing';
+import Detail from "./views/Detail/Detail";
+import Form from "./views/Form/Form"; 
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+
+      <Route exact path="/" render={() => <Landing />} /> 
+      <Route exact path="/detail" render={() => <Detail />} /> 
+      <Route exact path="/create" render={() => <Form />} /> 
+     
+      <Route path="/home" render={() => <Home />} />
+
     </div>
   );
 }
