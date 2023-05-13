b6f3731eb4d1ac935efcbaf29b53dde010c9cbd0
MercedesPoisson <mercedespoisson@yahoo.com>
2023-05-13 20:31:12 -0300

    Filtro por tipo de dieta OK

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -8,52 +8,30 @@ const initialState = {
   };
   
   const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
+    switch (action.type) { 
       case GET_RECIPES:
-        return {
-          ...state,
-          recipes: action.payload,
-          allRecipes: action.payload
-        };
+        return { ...state, recipes: action.payload, allRecipes: action.payload };
       case GET_BY_NAME:
-        return {
-          ...state,
-          recipes: action.payload
-        };
+        return { ...state, recipes: action.payload };
       case GET_BY_ID:
         return {
-          ...state,
-          detail: action.payload
-        };
+          ...state, detail: action.payload };
       case CLEAN_DETAIL:
-        return {
-          ...state,
-          detail: {}
-        };
+        return { ...state, detail: {} };
       case GET_TYPE_DIETS:
         return {
-          ...state,
-          typeDiets: action.payload
-        };
-      case FILTER_BY_DIET:
+          ...state, typeDiets: action.payload };
+      case FILTER_BY_DIET:  
         const pointer = action.payload;
         let recipesFilter;
         if (pointer === "All Diet Types") {
           recipesFilter = [...state.allRecipes];
         } else {
           recipesFilter = state.allRecipes.filter(recipe =>
-            recipe.diets && recipe.diets.includes(pointer)
-          );
-        }
-        return {
-          ...state,
-          recipes: recipesFilter,
-          isFiltered: true
-        };
+            recipe.diets && recipe.diets.includes(pointer) )}
+        return { ...state, recipes: recipesFilter, isFiltered: true };
       case "POST_DIETS":
-        return {
-          ...state
-        };
+        return { ...state };
       default:
         return state;
     }
